5f3d7f532f7e0a340ba4d13f33087bdcc0f1b9e1
java007hf <102wangyingli@163.com>
2025-05-24 18:42:18 +0800

    调整布局

--- MyCar/app/src/main/java/com/example/mycar/MainActivity.kt
+++ MyCar/app/src/main/java/com/example/mycar/MainActivity.kt
@@ -87,8 +87,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
